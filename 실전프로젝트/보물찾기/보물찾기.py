936fd6c11f91c1e335b6c59a9f4292ba8fb05e10
SUNMI <hariaus@naver.com>
2025-10-04 04:36:01 +0900

    TIL: 보물찾기 코드 수정

--- 실전프로젝트/보물찾기/보물찾기.py
+++ 실전프로젝트/보물찾기/보물찾기.py
@@ -11,12 +11,9 @@ direction_map = {
 dx = {'F': 1, 'B' : -1, 'R' : 0, 'L' : 0}
 dy = {'F': 0, 'B' : 0, 'R' : 1, 'L' : -1}
 
-match_direction = {'F': 0, 'B' : 1, 'R' : 2, 'L' : 3}
-match_number = {0 : "F", 1 : "B", 2 : "R", 3 : "L"}
-
 from collections import deque
 
-'''def solve(m, visited):
+def solve(m, visited):
     queue = deque()
     queue.append((0, 0, 'B'))
 
@@ -31,24 +28,8 @@ from collections import deque
         if visited[x][y][nd] > 1:
             return (x, y)
 
-        queue.append((nx, ny, nd))'''
-
-def solve(m, visited):
-    queue = deque()
-    queue.append((0, 0, 'B'))
-
-    while queue:
-        x, y, d = queue.popleft()
-        md, count = m[x][y][0], int(m[x][y][1])
-        
-        nd = match_number[(match_direction[md] + match_direction[d]) % 4]
-        nx, ny = x + dx[nd] * count, y + dy[nd] * count
-
-        visited[x][y][nd] += 1
-        if visited[x][y][nd] > 1:
-            return (x, y)
-
         queue.append((nx, ny, nd))
+        
             
 t = int(input())
 for _ in range(t):
