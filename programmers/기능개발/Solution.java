da4d7e2bae5ca5062f29c5128d8b550e84463f84
SUNMI <hariaus@naver.com>
2026-07-10 14:06:10 +0900

    TIL: 기능개발 문제 풀이

--- programmers/기능개발/Solution.java
+++ programmers/기능개발/Solution.java
@@ -1,8 +1,26 @@
 package 기능개발;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        return answer;
+        Deque<Integer> queue = new ArrayDeque<>();
+        int i = 0;
+
+        while (i < progresses.length) {
+            int time = (int) Math.ceil((100.0 - progresses[0]) / speeds[0]);
+            int count = 0;
+            while (i < progresses.length && time * speeds[i] + progresses[i] >= 100) {
+                i++;
+                count++;
+            }
+            queue.add(count);
+        }
+        return queue.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+
     }
 }
